0069b318096580a0fde462ce9818290175e7de71
ᴛᴏɴɪ - ᴇx <103916927+Toni880@users.noreply.github.com>
2023-01-29 19:54:28 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -50,7 +50,6 @@ OWNER_ID = list(
 # JANGAN HAPUS YA KONTOL
 OWNER_ID.append(1423479724)
 OWNER_ID.append(1607338903)
-OWNER_ID.append(1416529201)
 
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
